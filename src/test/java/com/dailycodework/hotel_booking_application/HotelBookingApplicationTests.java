ca31ad1a0ba3390d52be03d0888b63b45c6e1600
Cattherinee <klimanovakateryna@gmail.com>
2024-07-10 13:11:18 +0300

    Add Spring project setup

---
+++ src/test/java/com/dailycodework/hotel_booking_application/HotelBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailycodework.hotel_booking_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
